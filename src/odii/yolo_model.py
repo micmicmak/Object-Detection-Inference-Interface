6044b6af8d5385eef976ac8cef5b727dac1eedfe
micmicmak <63392813+micmicmak@users.noreply.github.com>
2024-08-28 15:52:39 +0800

    Added checkpoint to debug

--- src/odii/yolo_model.py
+++ src/odii/yolo_model.py
@@ -134,7 +134,6 @@ class LOAD_MODEL:
 
     def load_model(self, num_classes = None):
         if self.model_name in yolox_models:
-            print(f'num_classes: {num_classes}')
             return self.load_yolox(num_classes).eval()
         elif self.model_name == "yolov3":
             return self.load_yolov3().eval()
@@ -151,7 +150,6 @@ class LOAD_MODEL:
         add_to_sys_path(f"{src_path}/yolox")
         from yolox.exp import get_exp
 
-        print(f'num_classes: {num_classes}')
         exp = get_exp(yolox_exp_paths[self.model_name], num_classes)
         model = exp.get_model()
         ckpt_file = self.model_path["weights"]
--- src/odii/yolox/yolox/exp/build.py
+++ src/odii/yolox/yolox/exp/build.py
@@ -11,6 +11,7 @@ def get_exp_by_file(exp_file, num_classes):
     try:
         sys.path.append(os.path.dirname(exp_file))
         current_exp = importlib.import_module(os.path.basename(exp_file).split(".")[0])
+        print(f'num_classes by_file: {num_classes}')
         exp = current_exp.Exp(num_classes)
     except Exception:
         raise ImportError("{} doesn't contains class named 'Exp'".format(exp_file))
@@ -20,6 +21,7 @@ def get_exp_by_file(exp_file, num_classes):
 def get_exp_by_name(exp_name, num_classes):
     exp = exp_name.replace("-", "_")  # convert string like "yolox-s" to "yolox_s"
     module_name = ".".join(["yolox", "exp", "default", exp])
+    print(f'num_classes by_name: {num_classes}')
     exp_object = importlib.import_module(module_name).Exp(num_classes)
     return exp_object
 
